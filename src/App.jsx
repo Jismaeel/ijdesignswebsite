527530cdff709ac2551a94b1438006ded364438c
Jismaeel <146990352+Jismaeel@users.noreply.github.com>
2024-06-20 21:42:31 +0200

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layout/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
-import PriceingPage from "./pages/PriceingPage";
+import PricingPage from "./pages/PriceingPage";
 
 const App = () => {
- 
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <BrowserRouter
-        basename="/ijdesignswebsite/"
-        path="/"
-        element={<MainLayout />}
-      >
-        <Route index element={<HomePage />} />
-        <Route path="/pricing" element={<PriceingPage />} />
-        <Route path="*" element={<NotFound />} />
-      </BrowserRouter>
-    )
+  return (
+    <BrowserRouter basename="/ijdesignswebsite/">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-
-  return <RouterProvider router={router} />;
 };
 
 export default App;
