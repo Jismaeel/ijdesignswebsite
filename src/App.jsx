f7a59cb384884bf47945a0ee0c801a81fafbff18
Jismaeel <146990352+Jismaeel@users.noreply.github.com>
2024-06-21 19:59:46 +0200

    base url added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ProjectsPage from "./pages/ProjectsPage";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/ijdesigns.online/">
+    <BrowserRouter basename="http://ijdesigns.online/">
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
